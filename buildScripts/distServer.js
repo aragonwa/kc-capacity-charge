4ed7aa35edcc0c40c15ed48a6da099f9ac62281f
Alex Aragon <Alex.Aragon@kingcounty.gov>
2017-08-21 13:14:50 -0700

    Adding receipt page

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -21,7 +21,7 @@ app.locals.env = process.env;
 app.use(compression());
 app.use(express.static(path.join(__dirname,'../dist')));
 
-let routes = ['index', 'review'];
+let routes = ['index', 'review', 'receipt'];
 for(let route of routes) {
   if(route === 'index') {
     app.get('/', (req, res) => {
